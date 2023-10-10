cce825a1fdd79ffeb9dce8fb8263e8df5054407c
Shengyang (Steven) Wu <70414253+StevenWu2001@users.noreply.github.com>
2023-10-10 18:41:46 -0400

    Updated guide for music commands

--- commands/guide.js
+++ commands/guide.js
@@ -14,8 +14,10 @@ module.exports = {
 
         var apex = "1: Player Stats :bulb:\n`!apex player ORIGIN_USERNAME`\n\n";
 
-        var music = "1: Play Music :musical_note:\n `!play SONG_NAME / LINK`\n\n";
-        music = "2: Disconnecting the Bot :musical_note:\n `!quit`\n\n";
+        var music = "1: Play Music (or add a new song to the playlist) :musical_note:\n `!play SONG_NAME / LINK`\n\n";
+        music = "2: Show the Current Playlist :musical_note:\n `!queue`\n\n";
+        music = "2: Skip the Currente Song :musical_note:\n `!next`\n\n";
+        music = "4: Disconnecting the Bot :musical_note:\n `!quit`\n\n";
 
 		const helpEmbed = new Discord.EmbedBuilder();
         helpEmbed.setColor('#0099ff');
